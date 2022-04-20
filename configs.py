9199e5a07f5bee47003783a8c2287521be40b492
Anonymous <88927572+DKBOTZHELP@users.noreply.github.com>
2022-04-20 15:30:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -16,6 +16,7 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    OWNER_ID = os.environ.get("OWNER_ID")
     SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "DK_BOTZ")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "MediaInfosBot")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
